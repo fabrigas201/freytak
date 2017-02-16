b202a4cce952fcad78f5b10e9042b05f99b70859
fabrigas201 <fabrigas201@gmail.com>
2017-02-16 18:01:59 +0300

    articlesModel

--- cms/Api/ArticlesModel.php
+++ cms/Api/ArticlesModel.php
@@ -21,10 +21,17 @@ class ArticlesModel{
 		
 		$sql .= ' ORDER BY `an`.`id` DESC ';
 		
+		
 		if (isset($data['start']) || isset($data['limit'])) {
-			if ($data['start'] < 0) {
+			if(!isset($data['start'])){
 				$data['start'] = 0;
+			}else{
+				if ($data['start'] < 0) {
+					$data['start'] = 0;
+				}
 			}
+			
+			
 
 			if ($data['limit'] < 1) {
 				$data['limit'] = 20;
